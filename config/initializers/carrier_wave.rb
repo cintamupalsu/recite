60b7d4101499593e4f9c5d8105c358307abd6751
a_maulana <cintamupalsu@hotmail.com>
2017-08-30 06:19:30 +0900

    Microposts done2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider => 'AWS',
-      :aws_access_key_id => ENV['AKIAIBAP4BKZJ5WDI5ZQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id => ENV['AKIAIRPV6XTR5GBI2JOQ'],
+      :aws_secret_access_key => ENV['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']
     }
     config.fog_directory = ENV['openhadith']
   end
