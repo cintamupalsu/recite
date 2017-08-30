ccf71f2f427dc8ea636d0db3a477c045b5df0634
a_maulana <cintamupalsu@hotmail.com>
2017-08-30 20:19:19 +0900

    aws01

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['AKIAIRPV6XTR5GBI2JOQ'],
-      :aws_secret_access_key => ENV['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']
+      provider:               'AWS',
+      aws_access_key_id:      ENV['AKIAIRPV6XTR5GBI2JOQ'],
+      aws_secret_access_key:  ENV['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']
     }
     config.fog_directory = ENV['openhadith']
   end
